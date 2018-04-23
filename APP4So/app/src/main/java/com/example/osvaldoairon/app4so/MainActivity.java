c8dfad844eb3ca50df9d780a00f4cd985db6685a
unknown <osvaldo_cavalcanti_filho@hotmail.com>
2018-04-22 21:46:27 -0300

    navbar

--- APP4So/app/src/main/java/com/example/osvaldoairon/app4so/MainActivity.java
+++ APP4So/app/src/main/java/com/example/osvaldoairon/app4so/MainActivity.java
@@ -5,11 +5,18 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.osvaldoairon.app4so.Fragments.MapGoogleActivity;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -18,12 +25,53 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity{
 
-    private static FusedLocationProviderClient fusedLocationProviderClient;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggler;
+    private MapGoogleActivity mapFragmento;
+    private FragmentManager fragmentManager;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        actionBarDrawerToggler = new ActionBarDrawerToggle(this,drawerLayout,R.string.Abr,R.string.Fec);
+        actionBarDrawerToggler.setDrawerIndicatorEnabled(true);
+
+
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggler);
+        actionBarDrawerToggler.syncState();
+
+        NavigationView view = (NavigationView)findViewById(R.id.nav_Mapa);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //carrega o botao home no actionbar
+
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if(id == R.id.config){
+                    Toast.makeText(MainActivity.this, "OKS", Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
+
+        fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fg = fragmentManager.beginTransaction();
+
+        fg.add(R.id.frameLayout,new MapGoogleActivity(), "MapGOOGLE");
+        fg.commitAllowingStateLoss();
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return  actionBarDrawerToggler.onOptionsItemSelected(item)||super.onOptionsItemSelected(item);
+    }
 }
